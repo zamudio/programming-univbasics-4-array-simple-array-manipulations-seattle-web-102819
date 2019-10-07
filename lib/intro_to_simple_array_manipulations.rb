929d9879d56252ad283a0fa0b487bb567dca72aa
Michael Z <mziacm@gmail.com>
2019-10-07 05:38:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,17 +31,17 @@ def using_insert(array, element)
 end
 
 def using_uniq(array)
-  array.
+  array.uniq
 end
 
 def using_flatten(array)
-  ...
+  array.flatten
 end
 
 def using_delete(array, string)
-  ...
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
-  ...
+  array.delete(integer)
 end
