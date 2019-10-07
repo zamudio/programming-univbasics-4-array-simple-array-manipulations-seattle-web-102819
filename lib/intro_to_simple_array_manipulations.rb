6621e684c8326cbba247f00b0dcea1d5ac0cdc72
Michael Z <mziacm@gmail.com>
2019-10-07 05:37:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,42 +1,37 @@
 def using_push(array, string)
-  ...
+  array.push(string)
 end
 
 def using_unshift(array, string)
-  ...
-  ...
+  array.unshift(string)
 end
 
 def using_pop(array)
-  ...
-  ...
+  array.pop
 end
 
 def pop_with_args(array)
-  ...
+  
 end
 
 def using_shift(array)
-  ...
-  ...
+  array.shift
 end
 
 def shift_with_args(array)
-  ...
+  
 end
 
 def using_concat(array1, array2)
-  ...
-  ...
+  array1 + array2
 end
 
 def using_insert(array, element)
-  ...
-  ...
+  array.insert(element)
 end
 
 def using_uniq(array)
-  ...
+  array.
 end
 
 def using_flatten(array)
