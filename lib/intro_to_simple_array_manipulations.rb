cfec1c9e190d0a4552fc01d249ad45f132792018
Michael Z <mziacm@gmail.com>
2019-10-07 05:41:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(element)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
@@ -43,5 +43,5 @@ def using_delete(array, string)
 end
 
 def using_delete_at(array, integer)
-  array.delete[integer]
+  array.delete(integer)
 end
